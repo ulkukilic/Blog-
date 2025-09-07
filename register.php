1de60dc3a60846d9730b3e4bf7870fbd2d400dc7
ulkukilic <ukilic1@stu.vistula.edu.pl>
2025-09-07 03:15:58 +0200

    add login and register pages

--- register.php
+++ register.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="/Blog-/css/style.css">
 </head>
 <body>
-<section class="login-container">
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<section class="register-container">
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img src="/Blog-/img/logo.png" alt="Your Company" class="mx-auto h-10 w-auto not-dark:hidden" />
     <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">Sign in to your account</h2>
@@ -18,6 +18,13 @@
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form action="#" method="POST" class="space-y-6">
+        <div>
+            <label for="full-name" class="block text-sm/6 font-semibold text-gray-900 dark:text-white">First name</label>
+            <div class="mt-2.5">
+            <input id="full-name" type="text" name="full-name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
+            </div>
+        </div>
+
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900 dark:text-gray-100">Email address</label>
         <div class="mt-2">
@@ -29,7 +36,6 @@
         <div class="flex items-center justify-between">
           <label for="password" class="block text-sm/6 font-medium text-gray-900 dark:text-gray-100">Password</label>
           <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Forgot password?</a>
           </div>
         </div>
         <div class="mt-2">
@@ -43,8 +49,7 @@
     </form>
 
     <p class="mt-10 text-center text-sm/6 text-gray-500 dark:text-gray-400">
-      Not a member?
-      <a href="register.php" class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Register Now </a>
+      <a href="login.php" class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"> You have already registered.</a>
     </p>
   </div>
 </div>
