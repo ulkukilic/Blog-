d4d141c8815db565cd42c19c4b1d49939ed3c95e
ulkukilic <ukilic1@stu.vistula.edu.pl>
2025-09-07 02:57:50 +0200

    update about Us page

--- customer/aboutUs.php
+++ customer/aboutUs.php
@@ -9,6 +9,105 @@
 </head>
 <body>
 <?php include __DIR__ . '/../layouts/header.php'; ?>
+<section class="aboutUs-container">
+<div class = "aboutUs" >
+<section class="bg-white dark:bg-gray-900 py-16 mt-20 ">
+  <div class="max-w-7xl mx-auto px-10 lg:px-8 grid lg:grid-cols-2 gap-15 items-center ">
+    <div>
+      <h2 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
+        About Us
+      </h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">
+        Since 2000, we’ve been dedicated to creating the best digital experiences. 
+        We love what we do and are proud to have connected thousands of people and ideas. 
+        Our team works with passion and joy — and we’re excited for what’s ahead.
+      </p>
+    </div>
+
+    <div class="grid grid-cols-2 gap-6">
+      <img src="/Blog-/img/about1.png" alt="Team working"
+        class="rounded-xl shadow-lg object-cover w-full h-48" />
+      <img src="/Blog-/img/blog2.png" alt="Collaboration"
+        class="rounded-xl shadow-lg object-cover w-full h-48" />
+      <img src="/Blog-/img/blog3.png" alt="Team meeting"
+        class="rounded-xl shadow-lg object-cover w-full h-48" />
+      <img src="/Blog-/img/about2.png" alt="Discussion"
+        class="rounded-xl shadow-lg object-cover w-full h-48" />
+    </div>
+
+  </div>
+</section>
+
+</div>
+
+<div class="withCompany">
+<div class="bg-white py-24 sm:py-32 dark:bg-gray-900">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <h2 class="text-center text-lg/8 font-semibold text-gray-900 dark:text-white">Trusted by the world’s most innovative teams</h2>
+    <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-gray-900.svg" alt="Transistor" class="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:hidden" />
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-white.svg" alt="Transistor" class="col-span-2 max-h-12 w-full object-contain not-dark:hidden lg:col-span-1" />
+
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-gray-900.svg" alt="Reform" class="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:hidden" />
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-white.svg" alt="Reform" class="col-span-2 max-h-12 w-full object-contain not-dark:hidden lg:col-span-1" />
+
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-gray-900.svg" alt="Tuple" class="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:hidden" />
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-white.svg" alt="Tuple" class="col-span-2 max-h-12 w-full object-contain not-dark:hidden lg:col-span-1" />
+
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-gray-900.svg" alt="SavvyCal" class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1 dark:hidden" />
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-white.svg" alt="SavvyCal" class="col-span-2 max-h-12 w-full object-contain not-dark:hidden sm:col-start-2 lg:col-span-1" />
+
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-gray-900.svg" alt="Statamic" class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1 dark:hidden" />
+      <img width="158" height="48" src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-white.svg" alt="Statamic" class="col-span-2 col-start-2 max-h-12 w-full object-contain not-dark:hidden sm:col-start-auto lg:col-span-1" />
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="workWithUs">
+ <div class="relative isolate overflow-hidden bg-white py-24 sm:py-32 dark:bg-gray-900">
+  <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=screen" alt="" class="absolute inset-0 -z-10 size-full object-cover object-right opacity-10 md:object-center dark:hidden" />
+  <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 size-full object-cover object-right not-dark:hidden md:object-center" />
+  <div aria-hidden="true" class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
+    <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-15 dark:opacity-20"></div>
+  </div>
+  <div aria-hidden="true" class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:-top-112 sm:ml-16 sm:translate-x-0">
+    <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-15 dark:opacity-20"></div>
+  </div>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:mx-0">
+      <h2 class="text-5xl font-semibold tracking-tight text-gray-200 sm:text-7xl dark:text-white">Work with us</h2>
+      <p class="mt-8 text-lg font-medium text-pretty text-gray-200 sm:text-xl/8 dark:text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.</p>
+    </div>
+    <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+      <div class="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-gray-200 sm:grid-cols-2 md:flex lg:gap-x-10 dark:text-white">
+        <a href="#">Open roles <span aria-hidden="true">&rarr;</span></a>
+        <a href="#">Meet our leadership <span aria-hidden="true">&rarr;</span></a>
+      </div>
+      <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+        <div class="flex flex-col-reverse gap-1">
+          <dt class="text-base/7 text-gray-400 dark:text-gray-300">Offices worldwide</dt>
+          <dd class="text-4xl font-semibold tracking-tight text-gray-400 dark:text-white">12</dd>
+        </div>
+        <div class="flex flex-col-reverse gap-1">
+          <dt class="text-base/7 text-gray-400 dark:text-gray-300">Full-time colleagues</dt>
+          <dd class="text-4xl font-semibold tracking-tight text-gray-400 dark:text-white">300+</dd>
+        </div>
+        <div class="flex flex-col-reverse gap-1">
+          <dt class="text-base/7 text-gray-400 dark:text-gray-300">Hours per week</dt>
+          <dd class="text-4xl font-semibold tracking-tight text-gray-300 dark:text-white">40</dd>
+        </div>
+        <div class="flex flex-col-reverse gap-1">
+          <dt class="text-base/7 text-gray-400 dark:text-gray-300">Paid time off</dt>
+          <dd class="text-4xl font-semibold tracking-tight text-gray-300 dark:text-white">Unlimited</dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</div>
+</div>
+
+</section>
 <?php include __DIR__ . '/../layouts/footer.php'; ?>
 </body>
 </html>
